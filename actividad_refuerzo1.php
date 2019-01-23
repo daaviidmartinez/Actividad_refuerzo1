48278b46000ce73ee0abdeea606399eb7ab115c5
ALUMNADO\damaro <damaro@floridauniversitaria.es>
2019-01-23 11:58:49 +0100

    Actividad de refuerzo 1

---
+++ actividad_refuerzo1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+    <h2>Actividad Refuerzo 1</h2>
+    <?php
+    
+    for ($i=1; $i < 41; $i = $i+2){
+        echo "<li> $i </li>";
+    }
+    ?>
+</body>
+</html>
